85eb9b586d14ff4cced3733a2a944964a5a5b9a6
Marco Chavez <mxrcochxvez@protonmail.com>
2021-02-13 15:01:34 -0800

    adding frontend

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+    return(
+        <div>
+            <h1>I am working!</h1>
+        </div>
+    )
+}
+
+export default App;
+
+const appDiv = document.getElementById('app')
+render(<App />, appDiv)
