e5587059bcdfb1f8aca6172fe6265daf52b8d3d6
Marco Chavez <mxrcochxvez@protonmail.com>
2021-02-15 00:44:47 -0800

    add a serializer for room creation requests

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -5,3 +5,8 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'vote_to_skip')
